e27f9c9b13a1a4fe5417877c53273234afc41f3e
qumar <qumar.hussain@gmail.com>
2023-03-21 18:08:41 +0000

    latest code

--- ListHostedZone/lambdapy.tf
+++ ListHostedZone/lambdapy.tf
@@ -5,6 +5,14 @@ def get_ec2_instances():
     instances = ec2.describe_instances()
     return instances['Reservations']
 
+def get_instance_name(instance):
+    instance_tags = instance.get('Tags', [])
+    instance_name = next((tag for tag in instance_tags if tag['Key'] == 'Name'), None)
+    if instance_name:
+        return instance_name['Value']
+    else:
+        return ''
+
 def get_matching_hosted_zones(name):
     route53 = boto3.client('route53')
     response = route53.list_hosted_zones()
@@ -15,12 +23,12 @@ def get_matching_hosted_zones(name):
             matching_zones.append(zone)
     return matching_zones
 
-def get_matching_dns_records(zone_id, instance_name, instance_id):
+def get_matching_dns_records(zone_id, instance_name):
     route53 = boto3.client('route53')
     records = route53.list_resource_record_sets(HostedZoneId=zone_id, MaxItems='500')
     matching_records = []
     for record in records['ResourceRecordSets']:
-        if record['Type'] == 'A' and not record['Name'].startswith(instance_name or instance_id) and not record.get('AliasTarget'):
+        if record['Type'] == 'A' and not record['Name'].startswith(instance_name) and not record.get('AliasTarget'):
             matching_records.append(record)
     return matching_records
 
@@ -29,20 +37,14 @@ def lambda_handler(event, context):
     
     for reservation in instances:
         for instance in reservation['Instances']:
-            instance_id = instance['InstanceId']
-            instance_tags = instance.get('Tags', [])
-            instance_name = next((tag for tag in instance_tags if tag['Key'] == 'Name'), None)
-            if instance_name:
-                instance_name = instance_name['Value']
-            else:
-                instance_name = ''
-            print(f"EC2 Instance: {instance_name or instance_id}")
+            instance_name = get_instance_name(instance)
+            print(f"EC2 Instance: {instance_name}")
             
-            matching_zones = get_matching_hosted_zones(instance_name or instance_id)
+            matching_zones = get_matching_hosted_zones(instance_name)
             for zone in matching_zones:
                 zone_id = zone['Id'].split('/')[-1]
                 print(f"Hosted Zone: {zone['Name']}")
                 
-                matching_records = get_matching_dns_records(zone_id, instance_name, instance_id)
+                matching_records = get_matching_dns_records(zone_id, instance_name)
                 for record in matching_records:
                     print(f"Record Name: {record['Name']}")
