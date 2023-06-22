5af89d2d9d4018192706af1d1326c364352c0f6e
qumar <qumar.hussain@gmail.com>
2023-06-22 18:38:37 +0100

    code

--- RemoveUnusedAMI/Lambda.tf
+++ RemoveUnusedAMI/Lambda.tf
@@ -5,7 +5,13 @@ def get_ec2_instances(application, environment):
     ec2 = boto3.client('ec2')
     env_app = '*' + environment + '-' + application + '*'
     filters = [{'Name': 'tag:Name', 'Values': [env_app]}, {'Name': 'instance-state-name', 'Values': ['running']}]
-    instances = ec2.describe_instances(Filters=filters)['Reservations']
+    instances_info = ec2.describe_instances(Filters=filters)['Reservations']
+
+    instances = []
+    for res in instances_info:
+        instances.extend(res['Instances'])
+
+    print(f"Instances for {env_app}: {instances}")
     return instances
 
 def get_amis(application, environment):
@@ -13,6 +19,8 @@ def get_amis(application, environment):
     env_app = '*' + application + '-' + environment + '*'
     filters = [{'Name': 'tag:Name', 'Values': [env_app]}]
     amis = ec2.describe_images(Owners=['self'], Filters=filters)['Images']  # Assuming these are custom AMIs owned by your account.
+
+    print(f"AMIs for {env_app}: {amis}")
     return amis
 
 def get_amis_of_instances(instances, amis):
@@ -20,22 +28,28 @@ def get_amis_of_instances(instances, amis):
     non_matched_amis = amis.copy()
 
     for instance in instances:
-        ami_id = instance['Instances'][0]['ImageId']
+        ami_id = instance['ImageId']
         for ami in amis:
             if ami['ImageId'] == ami_id:
                 matched_amis.append(ami)
                 non_matched_amis.remove(ami)
 
+    print(f"Matched AMIs: {matched_amis}")
+    print(f"Non-matched AMIs: {non_matched_amis}")
     return matched_amis, non_matched_amis
 
-def delete_amis(amis):
+def delete_amis(matched_amis, non_matched_amis):
     ec2 = boto3.client('ec2')
     # Sort AMIs by creation date
-    sorted_amis = sorted(amis, key=lambda ami: parse(ami['CreationDate']))
-    # Only delete AMIs that were created before the most recent one
-    for ami in sorted_amis[:-1]:
-        response = ec2.deregister_image(ImageId=ami['ImageId'])
-        print(f"AMI {ami['ImageId']} deregistered.")
+    sorted_matched_amis = sorted(matched_amis, key=lambda ami: parse(ami['CreationDate']))
+    sorted_non_matched_amis = sorted(non_matched_amis, key=lambda ami: parse(ami['CreationDate']))
+    # Find the creation date of the latest matched AMI
+    latest_matched_ami_date = parse(sorted_matched_amis[-1]['CreationDate'])
+    # Only delete non-matched AMIs that were created before the latest matched AMI
+    for ami in sorted_non_matched_amis:
+        if parse(ami['CreationDate']) < latest_matched_ami_date:
+            response = ec2.deregister_image(ImageId=ami['ImageId'])
+            print(f"AMI {ami['ImageId']} deregistered.")
 
 def lambda_handler(event, context):
     application = event.get('application')
@@ -47,11 +61,7 @@ def lambda_handler(event, context):
         }
 
     try:
-        instances_info = get_ec2_instances(application, environment)
-        instances = []
-        for res in instances_info:
-            instances.extend(res['Instances'])
-
+        instances = get_ec2_instances(application, environment)
         amis = get_amis(application, environment)
 
         # Check if any instances or AMIs are found
@@ -66,15 +76,14 @@ def lambda_handler(event, context):
         matched_amis, non_matched_amis = get_amis_of_instances(instances, amis)
         print(f"Found {len(matched_amis)} matched AMI(s) and {len(non_matched_amis)} non-matched AMI(s)")
 
-        # Only delete AMIs if any matches are found
-        if matched_amis:
-            delete_amis(matched_amis)
-
+        # Only delete AMIs if there are non-matched ones
+        if non_matched_amis:
+            delete_amis(matched_amis, non_matched_amis)
+            print(f"Deleted older non-matched AMIs.")
+        
         return {
-            'matched_amis': [ami['ImageId'] for ami in matched_amis],
-            'non_matched_amis': [ami['ImageId'] for ami in non_matched_amis]
+            'message': 'Completed successfully.'
         }
-
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return {
