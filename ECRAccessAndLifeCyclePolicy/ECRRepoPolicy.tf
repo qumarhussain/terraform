93f660cd76eeb564ce7999f0f0b65f26136bc417
qumar <qumar.hussain@gmail.com>
2023-10-13 14:17:33 +0100

    Latest code

--- ECRAccessAndLifeCyclePolicy/ECRRepoPolicy.tf
+++ ECRAccessAndLifeCyclePolicy/ECRRepoPolicy.tf
@@ -1,4 +1,27 @@
 data "aws_iam_policy_document" "ecr_power_user" {
+  source = "AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_policy" "ecr_repository_policy" {
+  name        = "ecr-repository-policy"
+  description = "ECR repository permissions for the specified AWS account"
+  policy      = data.aws_iam_policy_document.ecr_power_user.json
+}
+
+resource "aws_ecr_repository" "my_repository" {
+  name = "my-ecr-repository"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository_policy" "my_repository_policy" {
+  repository = aws_ecr_repository.my_repository.name
+  policy     = aws_iam_policy.ecr_repository_policy.policy
+}
+
+
+data "aws_iam_policy_document" "ecr_power_user" {
   source_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
 
