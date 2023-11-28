ada7a002d859093fdb62f9fb6f84752aa25504ad
qumar <qumar.hussain@gmail.com>
2023-11-28 12:44:54 +0000

    latest code

--- CrossAccountOU/json_policy.tf
+++ CrossAccountOU/json_policy.tf
@@ -15,3 +15,32 @@
     }
   ]
 }
+
+{
+
+resource "aws_iam_role_policy" "example" {
+  role = aws_iam_role.example.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowOUs"
+        Effect    = "Allow"
+        Action    = "sts:AssumeRole"
+        Principal = {
+          AWS = "*"
+        }
+        Condition = {
+          StringEquals = {
+            "aws:PrincipalOrgID": "o-xxxxxxxxxx"
+          }
+          ForAnyValue:StringLike = {
+            "aws:ResourceOrgPaths": ["o-a1b2c3d4e5/r-ab12/ou-ab12-11111111/*"]
+          }
+        }
+      }
+    ]
+  })
+}
+
